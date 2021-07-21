192eb53add543273117fb7ac50aeeec7a478d8f0
maxime vincent <maxime.vincent1@u-psud.fr>
2021-07-21 19:18:15 +0200

    Structure de base de requests.php

---
+++ requests.php
@@ -0,0 +1,22 @@
+<?php
+
+require("php/login_manager.php");
+require("php/quiz_manager.php");
+
+start_session();
+
+// Paramètres
+$action = $_REQUEST["action"];
+
+if ($action === "") {
+  
+}
+else if ($action === "") {
+  
+}
+// Action non-répertoriée
+else {
+  
+}
+
+?>
